8b161944020a7a7bd74ead3cfb70c8831cbc33c8
Seamul <islamseamul.com>
2021-06-20 18:04:50 +0600

    changes so far

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
-
+import Header from './containers/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
      <h1>Hello World</h1>
     </div>
   );
